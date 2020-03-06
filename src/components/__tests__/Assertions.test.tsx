f5669f7c988a408abd921dc6a5d81d70fd2ca87f
Victor Malov <victor.malov@breeffy.com>
2020-03-06 19:36:54 +0300

    update assertion tests

--- src/components/__tests__/Assertions.test.tsx
+++ src/components/__tests__/Assertions.test.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
 import * as fontawesome from '@fortawesome/fontawesome-svg-core';
 import SvgIcon from '../SvgIcon';
-import React from 'react';
-import renderer from 'react-test-renderer';
+
 import { AssertionError } from '../../assert';
 
-const faCoffee = {
+const faCoffee: fontawesome.IconDefinition = {
   prefix: 'fas',
   iconName: 'coffee',
   icon: [
@@ -16,7 +17,7 @@ const faCoffee = {
   ]
 };
 
-const faCircle = {
+const faCircle: fontawesome.IconDefinition = {
   prefix: 'fas',
   iconName: 'circle',
   icon: [
@@ -35,17 +36,31 @@ beforeEach(() => {
 });
 
 afterEach(() => {
+  // @ts-ignore
   console.error.mockRestore();
 });
 
-describe('test assertions in FontAwesomeIcon component', () => {
+describe('test assertions in SvgIcon component', () => {
+  describe('when [prefix,iconName] is not registered in library', () => {
+    test('assertion error is thrown', () => {
+      let catchedAssertionError = false;
+      try {
+        render(<SvgIcon icon={['fas', 'users']} />);
+      } catch (error) {
+        if (error instanceof AssertionError) {
+          catchedAssertionError = true;
+        }
+      }
+
+      expect(catchedAssertionError).toBe(true);
+    });
+  });
   describe('when height or width props are specified without a size prop', () => {
     test('assertion error is thrown', () => {
       let catchedAssertionError = false;
       try {
-        renderer
-          .create(<SvgIcon icon={faCoffee} height={32} width={32} />)
-          .toJSON();
+        // @ts-ignore
+        render(<SvgIcon icon={faCoffee} height={32} width={32} />);
       } catch (error) {
         if (error instanceof AssertionError) {
           catchedAssertionError = true;
@@ -60,9 +75,8 @@ describe('test assertions in FontAwesomeIcon component', () => {
     test('assertion error is thrown', () => {
       let catchedAssertionError = false;
       try {
-        renderer
-          .create(<SvgIcon icon={faCoffee} size={64} height={32} width={32} />)
-          .toJSON();
+        // @ts-ignore
+        render(<SvgIcon icon={faCoffee} size={64} height={32} width={32} />);
       } catch (error) {
         if (error instanceof AssertionError) {
           catchedAssertionError = true;
@@ -78,9 +92,8 @@ describe('test assertions in FontAwesomeIcon component', () => {
       console.error('fuck');
       let catchedAssertionError = false;
       try {
-        renderer
-          .create(<SvgIcon icon={faCoffee} color="purple" foo="bar" />)
-          .toJSON();
+        // @ts-ignore
+        render(<SvgIcon icon={faCoffee} color="purple" foo="bar" />);
       } catch (error) {
         if (error instanceof AssertionError) {
           catchedAssertionError = true;
