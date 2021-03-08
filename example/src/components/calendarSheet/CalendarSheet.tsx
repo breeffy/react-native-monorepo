104b73468ed7c2eaa2af0e217395b75998e85924
Victor Malov <victor.malov@breeffy.com>
2021-03-08 20:19:00 +0300

    chore: remove external View and style

--- example/src/components/calendarSheet/CalendarSheet.tsx
+++ example/src/components/calendarSheet/CalendarSheet.tsx
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react';
-import { View, StyleSheet } from 'react-native';
 import { Calendar, CalendarProps } from '@breeffy/react-native-calendar';
 import type { CalendarMethods } from '@breeffy/react-native-calendar';
 import type { ViewStyleProp } from '../../../../src/types';
@@ -9,31 +8,18 @@ export type CalendarSheetProps = {
   onDaySelectionChange?: CalendarProps['onDaySelectionChange'];
 } & ViewStyleProp;
 
-export const CalendarSheet = ({
-  style,
-  onDaySelectionChange
-}: CalendarSheetProps) => {
+export const CalendarSheet = ({ onDaySelectionChange }: CalendarSheetProps) => {
   const calendarRef = useRef<CalendarMethods>(null);
 
   return (
-    <View style={[styles.container, style]}>
-      <Calendar
-        style={styles.calendar}
-        ref={calendarRef}
-        selectionMode="singleDay"
-        scrollMode="multipleMonths"
-        scrollModeDeceleration="fast"
-        monthsBefore={12}
-        monthsAfter={24}
-        onDaySelectionChange={onDaySelectionChange}
-      />
-    </View>
+    <Calendar
+      ref={calendarRef}
+      selectionMode="singleDay"
+      scrollMode="multipleMonths"
+      scrollModeDeceleration="fast"
+      monthsBefore={12}
+      monthsAfter={24}
+      onDaySelectionChange={onDaySelectionChange}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'rgba(255, 255, 255, 1)'
-  },
-  calendar: {}
-});
