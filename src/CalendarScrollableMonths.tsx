1fa4da77a35b72b21570f72064f4e0fca2892818
Victor Malov <victor.malov@breeffy.com>
2021-04-06 19:16:59 +0300

    perf: optimized config for pagination

--- src/CalendarScrollableMonths.tsx
+++ src/CalendarScrollableMonths.tsx
@@ -10,7 +10,11 @@ import { getIdFromCalendarYearAndMonth } from './helpers';
 import { useCalendarScroll } from './hooks/useCalendarScrollView';
 import { useCalendarInternal } from './hooks/useCalendarInternal';
 import { useCalendarAnimated } from './hooks';
-import { ScrollState, windowWidth } from './constants';
+import {
+  CalendarPerformanceProps,
+  ScrollState,
+  windowWidth
+} from './constants';
 import { calculateScrollProgress } from './worklets';
 import { round } from 'react-native-redash';
 import { getStartOfEveryMonth } from './utils';
@@ -30,124 +34,135 @@ export interface CalendarScrollableMonthsProps {
   scrollModeDeceleration: FlatListProps<any>['decelerationRate'];
   activeCalendarDay: CalendarDate;
   selectedDates: CalendarDate[];
+  performanceProps: CalendarProps['performanceProps'];
   onCalendarDayPress?: (day: CalendarDate, kind: CalendarDayKind) => void;
 }
 
 export const CalendarScrollableMonths = forwardRef<
   any,
   CalendarScrollableMonthsProps
->(({ scrollMode, scrollModeDeceleration }, ref) => {
-  const scrollModeProps = useMemoOne<Partial<FlatListProps<any>>>(() => {
-    if (scrollMode === 'oneMonth') {
-      return {
-        pagingEnabled: true
-      };
-    } else if (scrollMode === 'multipleMonths') {
-      return {
-        snapToInterval: windowWidth
-      };
-    } else {
-      return {};
-    }
-  }, [scrollMode]);
-
-  const { monthsBefore, calendarInterval } = useCalendarInternal();
-
-  const {
-    scrollRef,
-    scrollHandler,
-    scrollState,
-    scrollContentOffset,
-    scrollContentSize
-  } = useCalendarScroll();
-
-  // useImperativeHandle(ref, () => scrollRef.current);
-  useImperativeHandle(ref, () => scrollRef);
-
-  const {
-    calendarAnimatedCommonEraMonth,
-    calendarCurrentAnimatedMonthInYear,
-    calendarAnimatedScrollProgress,
-    calendarStartMonthFromCommonEra,
-    calendarEndMonthFromCommonEra
-  } = useCalendarAnimated();
-
-  useDerivedValue(() => {
-    if (scrollState.value !== ScrollState.UNDETERMINED) {
-      calendarAnimatedCommonEraMonth.value = round(
-        interpolate(
-          scrollContentOffset.value.x,
-          [0, scrollContentSize.value.width - windowWidth],
-          [calendarStartMonthFromCommonEra, calendarEndMonthFromCommonEra],
-          Extrapolate.CLAMP
-        ),
-        4
-      );
-
-      calendarCurrentAnimatedMonthInYear.value =
-        calendarAnimatedCommonEraMonth.value % 12;
-
-      calendarAnimatedScrollProgress.value = calculateScrollProgress(
-        calendarAnimatedCommonEraMonth,
-        [calendarStartMonthFromCommonEra, calendarEndMonthFromCommonEra]
-      );
-    }
-  }, [calendarStartMonthFromCommonEra, calendarEndMonthFromCommonEra]);
-
-  const calendarMonths = useMemoOne(() => {
-    return getStartOfEveryMonth(calendarInterval).map<CalendarYearAndMonth>(
-      (dt) => {
+>(
+  (
+    {
+      scrollMode,
+      scrollModeDeceleration,
+      performanceProps = CalendarPerformanceProps
+    },
+    ref
+  ) => {
+    const scrollModeProps = useMemoOne<Partial<FlatListProps<any>>>(() => {
+      if (scrollMode === 'oneMonth') {
         return {
-          year: dt.year,
-          month: dt.month
+          pagingEnabled: true
         };
+      } else if (scrollMode === 'multipleMonths') {
+        return {
+          snapToInterval: windowWidth
+        };
+      } else {
+        return {};
       }
+    }, [scrollMode]);
+
+    const { monthsBefore, calendarInterval } = useCalendarInternal();
+
+    const {
+      scrollRef,
+      scrollHandler,
+      scrollState,
+      scrollContentOffset,
+      scrollContentSize
+    } = useCalendarScroll();
+
+    // useImperativeHandle(ref, () => scrollRef.current);
+    useImperativeHandle(ref, () => scrollRef);
+
+    const {
+      calendarAnimatedCommonEraMonth,
+      calendarCurrentAnimatedMonthInYear,
+      calendarAnimatedScrollProgress,
+      calendarStartMonthFromCommonEra,
+      calendarEndMonthFromCommonEra
+    } = useCalendarAnimated();
+
+    useDerivedValue(() => {
+      if (scrollState.value !== ScrollState.UNDETERMINED) {
+        calendarAnimatedCommonEraMonth.value = round(
+          interpolate(
+            scrollContentOffset.value.x,
+            [0, scrollContentSize.value.width - windowWidth],
+            [calendarStartMonthFromCommonEra, calendarEndMonthFromCommonEra],
+            Extrapolate.CLAMP
+          ),
+          4
+        );
+
+        calendarCurrentAnimatedMonthInYear.value =
+          calendarAnimatedCommonEraMonth.value % 12;
+
+        calendarAnimatedScrollProgress.value = calculateScrollProgress(
+          calendarAnimatedCommonEraMonth,
+          [calendarStartMonthFromCommonEra, calendarEndMonthFromCommonEra]
+        );
+      }
+    }, [calendarStartMonthFromCommonEra, calendarEndMonthFromCommonEra]);
+
+    const calendarMonths = useMemoOne(() => {
+      return getStartOfEveryMonth(calendarInterval).map<CalendarYearAndMonth>(
+        (dt) => {
+          return {
+            year: dt.year,
+            month: dt.month
+          };
+        }
+      );
+    }, [calendarInterval]);
+
+    const renderItem = useCallbackOne<ListRenderItem<CalendarYearAndMonth>>(
+      ({ item }) => {
+        return <CalendarMonthWithContext calendarYearAndMonth={item} />;
+      },
+      []
     );
-  }, [calendarInterval]);
 
-  const renderItem = useCallbackOne<ListRenderItem<CalendarYearAndMonth>>(
-    ({ item }) => {
-      return <CalendarMonthWithContext calendarYearAndMonth={item} />;
-    },
-    []
-  );
+    const keyExtractor = useCallbackOne(
+      (item: CalendarYearAndMonth, _: number) => {
+        return getIdFromCalendarYearAndMonth(item);
+      },
+      []
+    );
 
-  const keyExtractor = useCallbackOne(
-    (item: CalendarYearAndMonth, _: number) => {
-      return getIdFromCalendarYearAndMonth(item);
-    },
-    []
-  );
+    const getItemLayout = useCallbackOne(
+      (_: CalendarYearAndMonth[] | null | undefined, index: number) => {
+        return {
+          length: windowWidth,
+          offset: windowWidth * index,
+          index
+        };
+      },
+      []
+    );
 
-  const getItemLayout = useCallbackOne(
-    (_: CalendarYearAndMonth[] | null | undefined, index: number) => {
-      return {
-        length: windowWidth,
-        offset: windowWidth * index,
-        index
-      };
-    },
-    []
-  );
-
-  return (
-    <AnimatedFlatList
-      // @ts-ignore
-      ref={scrollRef}
-      // ref={scrollRef}
-      data={calendarMonths}
-      initialScrollIndex={monthsBefore}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      scrollEventThrottle={1}
-      decelerationRate={scrollModeDeceleration}
-      // maxToRenderPerBatch={1}
-      windowSize={3}
-      onScroll={scrollHandler}
-      renderItem={renderItem}
-      keyExtractor={keyExtractor}
-      getItemLayout={getItemLayout}
-      {...scrollModeProps}
-    />
-  );
-});
+    return (
+      <AnimatedFlatList
+        // @ts-ignore
+        ref={scrollRef}
+        // ref={scrollRef}
+        data={calendarMonths}
+        initialScrollIndex={monthsBefore}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        decelerationRate={scrollModeDeceleration}
+        initialNumToRender={performanceProps.initialNumToRender}
+        windowSize={performanceProps.windowSize}
+        maxToRenderPerBatch={performanceProps.maxToRenderPerBatch}
+        scrollEventThrottle={performanceProps.scrollEventThrottle}
+        onScroll={scrollHandler}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        getItemLayout={getItemLayout}
+        {...scrollModeProps}
+      />
+    );
+  }
+);
