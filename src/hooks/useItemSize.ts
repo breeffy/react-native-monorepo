bb72e1dc73b91f0ee10f1e212e07b1c21a1b7413
Victor Malov <victor.malov@breeffy.com>
2021-09-11 19:27:29 +0300

    Document useItemSize hook

--- src/hooks/useItemSize.ts
+++ src/hooks/useItemSize.ts
@@ -7,6 +7,13 @@ interface Props {
   itemHeight: number;
 }
 
+/**
+ * Returns item size along the main axis. Main axis is defined by mode.
+ * @param mode How items are layed out relative to each other
+ * @param itemWidth width of the item
+ * @param itemHeight height of the item
+ * @returns size along the main axis (width or height depending on mode)
+ */
 export const useItemSize = ({ mode, itemWidth, itemHeight }: Props): number => {
   const size = useMemoOne(() => {
     return getCellSize(mode, itemWidth, itemHeight);
