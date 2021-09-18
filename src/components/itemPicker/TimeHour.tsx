2a6dac0b4c85d1436c35f3b5c61c5460a0dac633
Victor Malov <victor.malov@breeffy.com>
2021-09-18 16:07:41 +0300

    Remove unused variables and parameters

--- src/components/itemPicker/TimeHour.tsx
+++ src/components/itemPicker/TimeHour.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Text, StyleSheet, StyleProp, TextStyle } from 'react-native';
-import { useMemoOne } from 'use-memo-one';
 import { dayOfWeekWidth } from '../../constants';
-import { calendarDayThemeToTextStyle } from '../../helpers';
-import { useCalendarTheme } from '../../hooks/useCalendarTheme';
 
 export enum CalendarDayKind {
   DEFAULT,
@@ -19,40 +16,9 @@ export type TimeHourProps = {
   style?: StyleProp<TextStyle>;
 };
 
-export const TimeHour = ({
-  kind = CalendarDayKind.DEFAULT,
-  hour,
-  style
-}: TimeHourProps) => {
-  // console.log(`CalendarDay: kind: ${kind}, day: ${day}`);
-  const theme = useCalendarTheme();
-  const calendarDayTheme = useMemoOne(() => {
-    switch (kind) {
-      case CalendarDayKind.DEFAULT:
-        return theme.day.default;
-      case CalendarDayKind.DISABLED:
-        return theme.day.disabled;
-      case CalendarDayKind.ACTIVE:
-        return theme.day.active;
-      case CalendarDayKind.SELECTED:
-        return theme.day.selected;
-    }
-  }, [kind, theme]);
-
-  const textStyle = useMemoOne(() => {
-    return [
-      styles.container,
-      styles.text,
-      calendarDayThemeToTextStyle(calendarDayTheme),
-      style
-    ];
-  }, [calendarDayTheme]);
-
-  // console.log(`textStyle is ${JSON.stringify(textStyle)}`);
-
+export const TimeHour = ({ hour }: TimeHourProps) => {
   return (
     <>
-      {/* <Text style={textStyle}>{String(hour)}</Text> */}
       <Text style={styles.container}>{String(hour)}</Text>
     </>
   );
