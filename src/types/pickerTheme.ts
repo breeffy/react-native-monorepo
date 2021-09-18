31705db5fef55d098b3590da073a06b175024ee7
Victor Malov <victor.malov@breeffy.com>
2021-09-18 15:46:10 +0300

    Remove unused variable

--- src/types/pickerTheme.ts
+++ src/types/pickerTheme.ts
@@ -1,6 +1,6 @@
 import type { ComponentProps } from 'react';
 import type { TextStyle } from 'react-native';
-import type { Circle, Line } from 'react-native-svg';
+import type { Line } from 'react-native-svg';
 
 type TextColor = NonNullable<TextStyle['color']>;
 type TextFont = NonNullable<TextStyle['fontFamily']>;
@@ -8,7 +8,6 @@ type TextSize = NonNullable<TextStyle['fontSize']>;
 type TextWeight = NonNullable<TextStyle['fontWeight']>;
 
 type BackgroundColor = NonNullable<TextStyle['backgroundColor']>;
-type CircleColor = NonNullable<ComponentProps<typeof Circle>['fill']>;
 
 type LineColor = NonNullable<ComponentProps<typeof Line>['stroke']>;
 type LineWidth = NonNullable<ComponentProps<typeof Line>['strokeWidth']>;
