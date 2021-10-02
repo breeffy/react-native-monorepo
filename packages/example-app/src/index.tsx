c71f2a83ee1f8ab22c34935f66efe07a99819b70
Victor Malov <victor.malov@breeffy.com>
2021-10-02 21:07:22 +0300

    Add native stack navigation

--- packages/example-app/src/index.tsx
+++ packages/example-app/src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import {
   // Image,
   Platform,
   SafeAreaView,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export function App() {
+const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.root}>
-      <Text style={styles.text}>Hello from React Native!</Text>
+      <Text style={styles.text}>Hello from React Native!!</Text>
       <View style={styles.platformRow}>
         <Text style={styles.text}>Platform: </Text>
         <View style={styles.platformBackground}>
@@ -20,41 +22,53 @@ export function App() {
       </View>
     </SafeAreaView>
   );
+};
+
+const Stack = createNativeStackNavigator();
+
+export function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   root: {
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white'
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
   },
   logo: {
     width: 120,
     height: 120,
-    marginBottom: 20
+    marginBottom: 20,
   },
   text: {
     fontSize: 28,
-    fontWeight: '600'
+    fontWeight: "600",
   },
   platformRow: {
     marginTop: 12,
-    flexDirection: 'row',
-    alignItems: 'center'
+    flexDirection: "row",
+    alignItems: "center",
   },
   platformValue: {
     fontSize: 28,
-    fontWeight: '500'
+    fontWeight: "500",
   },
   platformBackground: {
-    backgroundColor: '#ececec',
+    backgroundColor: "#ececec",
     borderWidth: StyleSheet.hairlineWidth,
-    borderColor: '#d4d4d4',
+    borderColor: "#d4d4d4",
     paddingHorizontal: 6,
     borderRadius: 6,
-    alignItems: 'center'
-  }
+    alignItems: "center",
+  },
 });
 
 export default App;
