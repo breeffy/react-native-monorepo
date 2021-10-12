1fcc718a0d970efd73e83f88f22fac8a645a3c4a
Victor Malov <victor.malov@breeffy.com>
2021-10-12 14:46:27 +0300

    config(eslint): Don't require React in scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
   },
   rules: {
     'jsx-quotes': ['error', 'prefer-single'],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
       'warn',
