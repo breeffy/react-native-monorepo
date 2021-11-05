69b7535d6538217d4c7b5235ce9e819801289e52
Victor Malov <victor.malov@breeffy.com>
2021-11-05 21:42:58 +0300

    [elements] Add new Layout component

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -5,3 +5,7 @@ export type { ToggleProps } from './components/toggles/Toggle';
 /** Export stateful Toggle component */
 export { ToggleWithState } from './components/toggles/ToggleWithState';
 export type { ToggleWithStateProps } from './components/toggles/ToggleWithState';
+
+/** Export Layout component */
+export { Layout } from './components/layouts';
+export type { LayoutProps } from './components/layouts';
